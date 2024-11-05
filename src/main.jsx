ca58d0c49732cd3140e7065356c80c175f940345
ryannarnoldd <ryannarnoldd@gmail.com>
2024-11-04 19:22:41 -0500

    Setting up project file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+// Todo: Bring in the appropriate imports
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <ErrorPage />,
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
