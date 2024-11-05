6e4f935c6e45dd58cc52d3b539ca7021eed16e8b
ryannarnoldd <ryannarnoldd@gmail.com>
2024-11-04 19:50:29 -0500

    Created working dom and componenets folder

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+            <li>
+                <a href="/">About</a>
+            </li>
+            <li>
+                <a href="/portfolio">Portfolio</a>
+            </li>
+            </ul>
+        </nav>
+    );
+  }
+  
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,44 @@ import ReactDOM from 'react-dom/client';
 // Todo: Bring in the appropriate imports
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 // Bringing in the pages the router will use to conditionally show the appropriate views
 import App from './App';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ResumePage from './pages/ResumePage';
 
 const router = createBrowserRouter([
   {
-    path: '/',
     element: <App />,
     // errorElement: <ErrorPage />,
-  },
+    children: [
+      // Both are on purpose.
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: '/about',
+        element: <AboutPage />
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />
+      },
+      {
+        path: '/portfolio',
+        element: <PortfolioPage />
+      },
+      {
+        path: '/resume',
+        element: <ResumePage />
+      }
+    ]
+  }
 ]);
 
 // Render the RouterProvider component
